3cde8af31c3d4c35a58b081dc807ff662b1df1b2
Riot <romanmissuryn@gmail.com>
2026-05-10 16:39:35 +0300

    wdasdadsssadassdasdsas

---
+++ python/template/docs/index.md
@@ -0,0 +1,7 @@
+# Documents for python-app
+This application has two endpoints:
+- `/api/v1/health`
+- `/api/v1/details`
+Here is how could expand on what each of those endpoints do.
+# How to access the app?
+You can access the app by accessing URL: `${{values.app_name}}-${{values.app_env}}.test.com/api/v1/health`
---
+++ python/template/src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route("/api/v1/details")
+
+def details():
+    return jsonify({
+        "time": datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        "hostname": socket.gethostname(),
+        "message": "Some text!!",
+        "I finally fucking": "made it",
+        "env": "${{values.app_env}}",
+        "app_name": "${{values.app_name}}"
+    })
+
+@app.route("/api/v1/health")
+
+def health():
+    return jsonify({
+        "status": "up"
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
